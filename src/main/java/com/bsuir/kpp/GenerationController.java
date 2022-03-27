abc38dea000ed940cb520c8de7409af9b0cc69d5
Hanna_Akhrameika <aneanra@yandex.ru>
2022-03-27 18:38:50 +0300

    final commit

--- src/main/java/com/bsuir/kpp/GenerationController.java
+++ src/main/java/com/bsuir/kpp/GenerationController.java
@@ -1,19 +1,20 @@
 package com.bsuir.kpp;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-public class Controller {
+public class GenerationController {
 
     @GetMapping("/less")
-    public int less(@RequestParam(value = "value") int value) {
-        return (int)(Math.random() * ++value);
-    }
-    @GetMapping("/more")
-    public int more(@RequestParam(value = "value") int value) {
-        return (int)(Math.random()*100)+value;
+    public GeneratedNumber generateLess(@RequestParam(value = "number", defaultValue = "0") int number) {
+        return new GeneratedNumber((int) (Math.random() * ++number));
+
     }
 
+    @GetMapping("/more")
+    public GeneratedNumber generateMore(@RequestParam(value = "number", defaultValue = "0") int number) {
+        return new GeneratedNumber((int) (Math.random() * 100 + number));
+    }
 }
--- src/main/java/com/bsuir/kpp/Number.java
+++
@@ -1,2 +0,0 @@
-package com.bsuir.kpp;public class Number {
-}
