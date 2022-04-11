88de4a25667f015189da5d1d1215896ffec6b600
Hanna_Akhrameika <aneanra@yandex.ru>
2022-04-11 23:00:34 +0300

    final commit

--- src/test/java/com/bsuir/kpp/KppApplicationTests.java
+++ src/test/java/com/bsuir/kpp/KppApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -12,7 +13,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,31 +31,27 @@ class KppApplicationTests {
 	@Test
 	public void testRNG_successfulResult_correctRequest() throws Exception {
 		MvcResult mvcResult = this.mockMvc.perform(get("/less?number=90")).andReturn();
-		assertEquals(200, mvcResult.getResponse().getStatus());
-		//with HttpStatus.OK doesn't work, cuz return 200 instead 200 OK
+		assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
 	}
 
 	@Test
 	public void testRNG_badRequest_wrongParam() throws Exception {
 		MvcResult mvcResult = this.mockMvc.perform(get("/less?number=-1")).andReturn();
-		assertEquals(400, mvcResult.getResponse().getStatus());
-		//with HttpStatus.BAD_REQUEST doesn't work cuz return 400 instead 400 BAD_REQUEST
+		assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
 	}
 
 	@Test
-	public void testLessRNG_SuccessResult() {
+	public void testLessRNG_successResult() {
 		int value = 20;
 		int actual = generationController.generateLess(value).getValue();
-		int expected = value;
-		assertTrue(actual <= expected);
+		assertTrue(actual <= value);
 	}
 
 	@Test
-	public void testMoreRNG_SuccessResult() {
+	public void testMoreRNG_successResult() {
 		int value = 20;
 		int actual = generationController.generateMore(value).getValue();
-		int expected = value;
-		assertTrue(actual >= expected);
+		assertTrue(actual >= value);
 	}
 
 }
